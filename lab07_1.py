2f2be4f3a69a819e816994b423f29ce7401c6d7c
MUHAMMAD QASIM KALEEM <mk2554@live.mdx.ac.uk>
2026-03-26 19:31:56 +0000

    Friday

--- lab07_1.py
+++ lab07_1.py
@@ -1,5 +1,7 @@
 import bcrypt
 import os
+import sqlite3
+#creating hash password function
 def hash_password(plain_text_password):
     password_bytes = plain_text_password.encode("utf-8")
     salt = bcrypt.gensalt()
@@ -7,7 +9,7 @@ def hash_password(plain_text_password):
     return(bcrypted_password.decode('utf-8'))
 
 
-
+#creating verify password function
 def verify_password(plain_text_password, hash_password_):
     password_bytes = plain_text_password.encode("utf-8")
     hash_password_bytes = hash_password_.encode("utf-8")
@@ -17,7 +19,7 @@ def verify_password(plain_text_password, hash_password_):
         return False
 
 
-
+#creating register user function and login user function
 def register_user():
     user_name=input('what is your name?')
     password=input('what is your password?')
@@ -26,7 +28,7 @@ def register_user():
         f.write(f"{user_name},{h_password}\n")
     print('User registered successfully!')
 
-
+#creating login user function 
 def login_user():
     user_name=input('what is your name?')
     password=input('what is your password?')
@@ -42,7 +44,7 @@ def login_user():
                 return False
                 print('Envalid user')
 
-
+#creating main menu
 while True:
     print('Welcome')
     print('1. Register')
@@ -60,4 +62,30 @@ while True:
     else:
         print('Invalid input !!!!!')
 
+#################
+def create_user_table(conn):
+    cursor=conn.cursor()
+    sql="""CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username Text NOT NULL UNIQUE, password_hash TEXT NOT NULL)"""
+    cursor.execute(sql)
+    conn.commit()
 
+
+def add_user(conn, name, hash_password):
+    cursor=conn.cursor()
+    sql="""INSERT INTO users(username,password_hash) VALUES (?, ?)"""
+    param=(name,hash_password)
+    cursor.execute(sql,param)
+    conn.commit()
+    
+
+
+conn=sqlite3.connect('DATA/intelligent_pattern.db')
+
+with open('DATA/users.txt', 'r') as f:
+    users = f.readlines()
+
+    for user in users:
+        store_name, store_hash = user.strip().split(',')
+        add_user(conn, store_name,store_hash)
+    
+conn.close()
